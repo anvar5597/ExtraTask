45610b196c9056358aba2e57f2459efbbe7cf2ad
anvar5597 <olimovanvar97@gmail.com>
2022-12-12 11:10:23 +0500

    12.12

--- src/onlineTest/OnlineTestDemo.java
+++ src/onlineTest/OnlineTestDemo.java
@@ -17,13 +17,13 @@ public class OnlineTestDemo {
         users.add(new User("Anvar", "admin", "12345", "1"));
         users.add(new User("Anvar", "olimov@gmail.com", "12345", "2"));
         List<Answer> answers = new ArrayList<>();
-        answers.add(new Answer("A.c^2 = a^2 + b^2",true));
-        answers.add(new Answer("A.c^2 = a^2 - b^2",false));
-        answers.add(new Answer("A.c^3 = a^3 - b^3",false));
+        answers.add(new Answer("A.c^2 = a^2 + b^2", true));
+        answers.add(new Answer("A.c^2 = a^2 - b^2", false));
+        answers.add(new Answer("A.c^3 = a^3 - b^3", false));
         List<Question> question = new ArrayList<>();
-        question.add(new Question("1.Pifagor formulasi" , answers,false,"Anvar"));
-        List<Subject> subjects=new ArrayList<>();
-        subjects.add(new Subject("Matematika",question,0));
+        question.add(new Question("1.Pifagor formulasi", answers, false, "Anvar"));
+        List<Subject> subjects = new ArrayList<>();
+        subjects.add(new Subject("Matematika", question, 0));
 
 // test
 //        subjects.add(new Subject("Matematika",questions,1));
@@ -63,10 +63,10 @@ public class OnlineTestDemo {
             boolean chek = scanner.nextBoolean();
             answerList.add(new Answer(javob, chek));
         }
+//        questionList.add(new Question("laskfjsalkdj", answerList, false, ""));
 //        answerList.add(new Answer("A. Text1", false));
 //        answerList.add(new Answer("B. Text2", true));
 //        answerList.add(new Answer("C. Text3", false));
-        questionList.add(new Question("laskfjsalkdj", answerList, false, ""));
 
 
     }
@@ -94,7 +94,29 @@ public class OnlineTestDemo {
     }
 
     private static void showAdminDashboard() {
+        System.out.println("-------Admin oynasi-------------");
+        System.out.println("1.Yangi mavzu yaratish");
+        System.out.println("2.Yangi test yaratish");
+        System.out.println("3.Testning javobini o`zgartirish");
+        System.out.println("7.Foydalanuvchilar ro`yhati");
+        System.out.println("8.Foydalanuvchilarning natijalarini ko`rish");
+        System.out.println("0.Chiqish");
+        int choose = scanner.nextInt();
+        switch (choose) {
+            case 1 -> addSubject();
+            case 2 -> addQuestion();
+            case 3 -> changeAnswer();
 
+        }
+    }
+
+    private static void changeAnswer() {
+    }
+
+    private static void addQuestion() {
+    }
+
+    private static void addSubject() {
     }
 
     private static void registration() {
